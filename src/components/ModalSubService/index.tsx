4910e35fe34f6cfc197f3a5ceb19576748e443cb
dawanlago <dawanlago1@gmail.com>
2022-11-30 09:09:00 -0300

    Feat: Criação do modal de lista de subServiços

---
+++ src/components/ModalSubService/index.tsx
@@ -0,0 +1,57 @@
+import React, {ReactNode} from 'react';
+import {
+  View,
+  Modal,
+  ModalProps,
+  KeyboardAvoidingView,
+  Text,
+  TouchableWithoutFeedback,
+  FlatList,
+} from 'react-native';
+import {ButtonAction} from '../ButtonAction';
+import {CardService} from '../CardService';
+import {Title} from '../Title';
+
+import {styles} from './styles';
+
+interface Detail {
+  description: string;
+  price: number;
+  time: string;
+}
+
+type Props = ModalProps & {
+  heightModal?: string;
+  data: [];
+  closeModal: () => void;
+};
+
+export const ModalSubService: React.FC<Props> = ({
+  heightModal,
+  data,
+  closeModal,
+  ...rest
+}: Props) => {
+  return (
+    <Modal transparent animationType="slide" statusBarTranslucent {...rest}>
+      <TouchableWithoutFeedback onPress={closeModal}>
+        <View style={styles.overlay}>
+          <KeyboardAvoidingView
+            behavior="padding"
+            keyboardVerticalOffset={250}
+            style={[styles.container, {height: heightModal || '45%'}]}>
+            <Text style={styles.title}>Que tal adicionar mais algo?</Text>
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={data}
+              renderItem={({item}: {item: Detail}) => (
+                <CardService details={item} click={() => {}} />
+              )}
+            />
+          </KeyboardAvoidingView>
+          <ButtonAction titleButton="Continuar sem adicionar" />
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+};
